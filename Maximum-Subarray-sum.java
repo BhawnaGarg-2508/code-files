99d9b70af37cadcf7d9225e06670ac4e54042240
BhawnaGarg-2508 <93832015+BhawnaGarg-2508@users.noreply.github.com>
2023-08-10 23:32:04 +0530

    Add files via upload

---
+++ Maximum-Subarray-sum.java
@@ -0,0 +1,36 @@
+
+import java.util.*; 
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements in array: ");
+		
+		int n = sc.nextInt();
+		
+		int [] arr = new int[n];
+	
+	    System.out.println("Enter the Values: ");
+		for(int i = 0; i< n; i++){
+		    arr[i] = sc.nextInt();
+		}
+		
+		int sum = 0;
+		int maxSum = 0;
+		
+		for(int i = 0; i< n; i++){
+		    sum += arr[i];
+		    if(sum > maxSum){
+		        maxSum = sum;
+		    }
+		    
+		    if(sum <0 ){
+		        sum = 0;
+		    }
+		}
+		
+		System.out.println("Maximum subarray sum is: " + maxSum);
+		
+		
+	}
+}
